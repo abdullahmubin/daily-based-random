29cb4882e41661d21cb253a1a97acac3fbb79b93
abdullahmubin <amubin19@gmail.com>
2023-02-28 22:05:17 +0600

    dynamic date added.

--- src/mainComponent/SubComponents/Dashboard.jsx
+++ src/mainComponent/SubComponents/Dashboard.jsx
@@ -17,6 +17,7 @@ import
 } from 'chart.js';
 import { Bar, Chart, Line } from 'react-chartjs-2';
 import faker from 'faker';
+import { useEffect } from 'react';
 
 ChartJS.register(
   CategoryScale,
@@ -28,7 +29,7 @@ ChartJS.register(
   TimeScale
 );
 
- const dateFormat = `YYYY-MM-DD HH:mm:ss`;
+const dateFormat = `YYYY-MM-DD HH:mm:ss`;
 
 // const config = {
 //   type: 'line',
@@ -189,106 +190,97 @@ const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 
 
 
-export const data = {
-  labels: ['modo 1', 'modo 2', 'modo 13'],
-  datasets: [{
-    label: "# Durata reale",
-    //fillColor: "rgba(51, 122, 183, 0.2)",
-    //strokeColor: "rgba(51, 122, 183, 0.6)",
-    //pointColor: "rgba(51, 122, 183, 0.6)",
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgba(255,99,132,1)',
+// let data = {
+//   labels: ['modo 1', 'modo 2', 'modo 13'],
+//   datasets: [{
+//     label: "# Durata reale",
+//     //strokeColor: "rgba(51, 122, 183, 0.6)",
+//     //pointColor: "rgba(51, 122, 183, 0.6)",
+//     backgroundColor: 'rgba(255, 99, 132, 0.2)',
+//     borderColor: 'rgba(255,99,132,1)',
+//     barThickness: 70,
+//     barPercentage: 0.9,
+//     categoryPercentage: 1,
+//     data: [6100, 615, 705]
+//   }, {
+//     label: "# Ore in",
+//     //fillColor: "rgba(241, 88, 84, 0.2)",
+//     //strokeColor: "rgba(241, 88, 84, 0.6)",
+//     //pointColor: "rgba(241, 88, 84, 0.6)",
+//     backgroundColor: '#DEF797',
+//     borderColor: 'rgba(255,99,132,1)',
 
-    // barPercentage: 0.8,
-    barThickness: 70,
-    // maxBarThickness: 8,
-    // minBarLength: 70,
-    // categorySpacing: 0,
-    // categoryPercentage: 0,
-    barPercentage: 0.9,
-    categoryPercentage: 1,
+//     // barPercentage: 0.8,
+//     barThickness: 70,
+//     // maxBarThickness: 8,
+//     // minBarLength: 70,
+//     // categorySpacing: 0,
+//     // categoryPercentage: 0,
+//     barPercentage: 0.9,
+//     categoryPercentage: 1,
 
-    // borderWidth: 1,
-    // hoverBackgroundColor: 'red',
+//     // borderWidth: 1,
+//     // hoverBackgroundColor: 'yellow',
+//     data: [0, 0, 465]
+//   }]
 
-    data: [6100, 615, 705]
-  }, {
-    label: "# Ore in",
-    //fillColor: "rgba(241, 88, 84, 0.2)",
-    //strokeColor: "rgba(241, 88, 84, 0.6)",
-    //pointColor: "rgba(241, 88, 84, 0.6)",
-    backgroundColor: '#DEF797',
-    borderColor: 'rgba(255,99,132,1)',
-
-    // barPercentage: 0.8,
-    barThickness: 70,
-    // maxBarThickness: 8,
-    // minBarLength: 70,
-    // categorySpacing: 0,
-    // categoryPercentage: 0,
-    barPercentage: 0.9,
-    categoryPercentage: 1,
-
-    // borderWidth: 1,
-    // hoverBackgroundColor: 'yellow',
-    data: [0, 0, 465]
-  }]
-
-  // labels: ["Day 1", "Day 2", "Day 3"],
-  // datasets: [{
-  //   label: 'working',
-  //   data: [
-  //     [new Date('2021-10-18T04:00:00'), new Date('2021-10-19T20:30:00')],
-  //     [new Date('2021-10-18T06:00:00'), new Date('2021-10-18T17:00:00')]
-  //   ],
-  //   backgroundColor: "red",
-  // },
-  // {
-  //   label: 'sleep',
-  //   data: [
-  //     [new Date('2021-10-18T00:00:00'), new Date('2021-10-18T04:00:00')],
-  //     [new Date('2021-10-18T00:00:00'), new Date('2021-10-18T06:00:00')],
-  //     [new Date('2021-10-18T00:00:00'), new Date('2021-10-18T06:00:00')]
-  //   ],
-  //   backgroundColor: "blue",
-  // },
-  // {
-  //   label: 'sleeping',
-  //   data: [
-  //     [new Date('2021-10-18T20:30:00'), new Date('2021-10-18T23:59:59')],
-  //     [new Date('2021-10-18T17:00:00'), new Date('2021-10-18T23:59:59')],
-  //     [new Date('2021-10-18T17:00:00'), new Date('2021-10-18T23:59:59')]
-  //   ],
-  //   backgroundColor: "orange",
-  // },
-  // ]
-  // labels,
-  // datasets: [
-  //   {
-  //     label: 'Dataset 1',
-  //     data: [100, 200, 300, 400, 500, 600, 111, 222, 333, 444, 555, 666],// labels.map(() => faker.datatype.number({ min: 100, max: 1000 })),
-  //     backgroundColor: 'rgb(255, 99, 132)',
-  //   },
-  //   {
-  //     label: 'Dataset 2',
-  //     data: [100, 200, 300, 400, 500, 600, 111, 222, 333, 444, 555, 666],
-  //     backgroundColor: 'rgb(75, 192, 192)',
-  //   },
-  //   {
-  //     label: 'Dataset 3',
-  //     data: [100, 200, 300, 400, 500, 600, 111, 222, 333, 444, 555, 666],
-  //     backgroundColor: 'rgb(53, 162, 235)',
-  //   },
-  // ],
-};
+//   // labels: ["Day 1", "Day 2", "Day 3"],
+//   // datasets: [{
+//   //   label: 'working',
+//   //   data: [
+//   //     [new Date('2021-10-18T04:00:00'), new Date('2021-10-19T20:30:00')],
+//   //     [new Date('2021-10-18T06:00:00'), new Date('2021-10-18T17:00:00')]
+//   //   ],
+//   //   backgroundColor: "red",
+//   // },
+//   // {
+//   //   label: 'sleep',
+//   //   data: [
+//   //     [new Date('2021-10-18T00:00:00'), new Date('2021-10-18T04:00:00')],
+//   //     [new Date('2021-10-18T00:00:00'), new Date('2021-10-18T06:00:00')],
+//   //     [new Date('2021-10-18T00:00:00'), new Date('2021-10-18T06:00:00')]
+//   //   ],
+//   //   backgroundColor: "blue",
+//   // },
+//   // {
+//   //   label: 'sleeping',
+//   //   data: [
+//   //     [new Date('2021-10-18T20:30:00'), new Date('2021-10-18T23:59:59')],
+//   //     [new Date('2021-10-18T17:00:00'), new Date('2021-10-18T23:59:59')],
+//   //     [new Date('2021-10-18T17:00:00'), new Date('2021-10-18T23:59:59')]
+//   //   ],
+//   //   backgroundColor: "orange",
+//   // },
+//   // ]
+//   // labels,
+//   // datasets: [
+//   //   {
+//   //     label: 'Dataset 1',
+//   //     data: [100, 200, 300, 400, 500, 600, 111, 222, 333, 444, 555, 666],// labels.map(() => faker.datatype.number({ min: 100, max: 1000 })),
+//   //     backgroundColor: 'rgb(255, 99, 132)',
+//   //   },
+//   //   {
+//   //     label: 'Dataset 2',
+//   //     data: [100, 200, 300, 400, 500, 600, 111, 222, 333, 444, 555, 666],
+//   //     backgroundColor: 'rgb(75, 192, 192)',
+//   //   },
+//   //   {
+//   //     label: 'Dataset 3',
+//   //     data: [100, 200, 300, 400, 500, 600, 111, 222, 333, 444, 555, 666],
+//   //     backgroundColor: 'rgb(53, 162, 235)',
+//   //   },
+//   // ],
+// };
 
 const Dashboard = () =>
 {
 
   const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  
-  const handleDateChange = (data) => {
+  const [endDate, setEndDate] = useState(moment(startDate, dateFormat).add(6, 'days').toLocaleString());
+  const [allData, setAllData] = useState(null)
+
+  const handleDateChange = (data) =>
+  {
     let [start, end] = data;
 
     let weekStart = moment(start).startOf('isoWeek').format(dateFormat);
@@ -297,23 +289,87 @@ const Dashboard = () =>
     setStartDate(weekStart)
     setEndDate(weekEnd)
 
+    console.log('endDate: ' + weekEnd)
+
     var now = weekStart, dates = [];
-    
-    while (moment(now).isSameOrBefore(moment(weekEnd))) {
-        dates.push(moment(now).format('M/D/YYYY'));
-        now = moment(now).add(1, 'days');
+
+    while (moment(now).isSameOrBefore(moment(weekEnd)))
+    {
+      dates.push(moment(now).format('M/D/YYYY'));
+      now = moment(now).add(1, 'days');
     }
-    
+
+    console.log('handle change');
+    console.log(dates)
+
   }
 
-  let s = moment(startDate, dateFormat).toLocaleString()// .toISOString();
-  let e = moment(startDate, dateFormat).add(6, 'days').toLocaleString()//.toISOString();
-  
+  useEffect(() =>
+  {
+    let weekStart = moment(startDate).startOf('isoWeek').format(dateFormat);
+    let weekEnd = moment(startDate).endOf('isoWeek').format(dateFormat);
+
+
+    var now = weekStart, dates = [];
+    let dateWithVal = []
+    while (moment(now).isSameOrBefore(moment(weekEnd)))
+    {
+      let currentDate = moment(now).format('M/D/YYYY');
+      dates.push(currentDate);
+
+      // let obj = {
+      //   label: "# Durata reale",
+      //   //strokeColor: "rgba(51, 122, 183, 0.6)",
+      //   //pointColor: "rgba(51, 122, 183, 0.6)",
+      //   backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      //   borderColor: 'rgba(255,99,132,1)',
+      //   barThickness: 70,
+      //   barPercentage: 0.9,
+      //   categoryPercentage: 1,
+      //   data: [6100, 615, 705]
+      // }
+
+      let randomNum = Math.floor(Math.random() * 100);
+      let currentDayWorkTime = 6100 + randomNum;
+
+      dateWithVal.push(currentDayWorkTime)
+
+      now = moment(now).add(1, 'days');
+    }
+    console.log('useEffect');
+    console.log(dates)
+
+    let dataSetList = [];
+    let obj = {
+      label: "# Durata reale",
+      //strokeColor: "rgba(51, 122, 183, 0.6)",
+      //pointColor: "rgba(51, 122, 183, 0.6)",
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      barThickness: 70,
+      barPercentage: 0.9,
+      categoryPercentage: 1,
+      data: dateWithVal
+    }
+    dataSetList.push(obj);
+    let allDataObj = {};
+
+    allDataObj.labels = dates;
+    allDataObj.datasets = dataSetList
+
+    setAllData(allDataObj)
+
+  }, []);
+
+  // let s = moment(startDate, dateFormat).toLocaleString()// .toISOString();
+  // let e = moment(startDate, dateFormat).add(6, 'days').toLocaleString()//.toISOString();
+
   return (
     <div>
 
       <div style={{ width: '700px' }}>
-        <Bar options={options} data={data} />
+        {
+          allData ? <Bar options={options} data={allData} /> : null}
       </div>
 
       <DatePicker
@@ -321,8 +377,8 @@ const Dashboard = () =>
         placeholderText="Select Week"
         className='form-control analytics-date-selector weekly-date-eod'
         onChange={handleDateChange}
-        startDate={new Date(s)}
-        endDate={new Date(e)}
+        startDate={new Date(startDate)}
+        endDate={new Date(endDate)}
         selected={new Date(moment(startDate, dateFormat).format('l'))}
         selectsRange
       />
